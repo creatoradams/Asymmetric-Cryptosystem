6a776c9014a842f0783560d0ed73da4bad4902ca
mmack <mmackenzie@cub.uca.edu>
2025-09-10 21:27:23 -0500

    seed for generating rsa keys

---
+++ starter.py
@@ -0,0 +1,14 @@
+class RSAKeyPair:
+    def __init__(self, n, e, d, p, q):
+        self.n = n
+        self.e = e
+        self.d = d
+        self.p = p
+        self.q = q
+
+def generate_seeds(bits):
+    while True:
+        p = generate_prime(bits //2)
+        q = generate_prime(bits // 2)
+        if p != q:
+            return {"p": p, "q": q}
